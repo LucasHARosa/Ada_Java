2325bcc57b6ef868a6bcfd4bf64ea15d4089775f
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-07-12 22:13:02 -0300

    teste

---
+++ src/aula1/Fahrenheit_Celcius.java
@@ -0,0 +1,20 @@
+package aula1;
+import java.util.Scanner;
+
+public class Fahrenheit_Celcius {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a temperatura em graus Fahrenheit: ");
+        double temperaturaFahrenheit = scanner.nextDouble();
+
+        double temperaturaCelsius = 5 * ((temperaturaFahrenheit - 32) / 9);
+
+        System.out.println("A temperatura em graus Celsius é: " + temperaturaCelsius);
+
+        scanner.close();
+
+    }
+}
