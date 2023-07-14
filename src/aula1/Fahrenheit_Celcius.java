bc5972a0a73265d21879f53d07b575791d235a47
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-07-14 20:01:13 -0300

    aula2-fruta

--- src/aula1/Fahrenheit_Celcius.java
+++ src/aula1/Fahrenheit_Celcius.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Fahrenheit_Celcius {
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite a temperatura em graus Fahrenheit: ");
