eaaa577a308f1c3d20a56569f2f200c42e88c586
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-07-17 20:26:11 -0300

    aula3 task02

---
+++ src/aula3/Ex03_02.java
@@ -0,0 +1,16 @@
+package aula3;
+
+public class Ex03_02 {
+    public static double[] custosCarro( double[] input) {
+        double[] saida = new double[2];
+        double valorFinal = input[0];
+        double distribuidor = input[1];
+        double imposto = input[2];
+        distribuidor = Math.round((distribuidor/valorFinal)*10000);
+        imposto = Math.round((imposto/valorFinal)*10000);
+        saida[0] = distribuidor/100;
+        saida[1] = imposto/100;
+        return saida;
+    }
+
+}
