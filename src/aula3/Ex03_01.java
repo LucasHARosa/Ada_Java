2a017261e9dc246d1d1828112d9891bef5afd78e
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-07-17 19:33:50 -0300

    aula3 task01

---
+++ src/aula3/Ex03_01.java
@@ -0,0 +1,22 @@
+package aula3;
+
+
+public class Ex03_01 {
+    public static void main(String[] args) {
+
+    }
+    public static double custoCompra(double input) {
+        double valorPromocao = 1.25;
+        double valorNormal = 1.45;
+        double valorTotal=0;
+        if(input>10){
+            valorTotal = input*valorPromocao;
+        }
+        else{
+            valorTotal = input*valorNormal;
+        }
+        return valorTotal;
+    }
+
+}
+
