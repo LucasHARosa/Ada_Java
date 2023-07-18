c2bb9396125ab2329c4365f7280ca8887ade8d9a
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-07-17 21:26:26 -0300

    Ex03 task06

--- src/aula3/Ex03_06.java
+++ src/aula3/Ex03_06.java
@@ -11,6 +11,7 @@ public class Ex03_06 {
             System.out.printf("lado %d: ", i+1);
             arestas[i] = scanner.nextDouble();
         }
+
         double area = calcularAreaTriangulo(arestas[0],arestas[1],arestas[2]);
         System.out.printf("A área é: %.2f",area);
     }
@@ -18,12 +19,21 @@ public class Ex03_06 {
     public static double calcularAreaTriangulo(double lado1, double lado2, double lado3) {
 
         double semiperimetro = (lado1 + lado2 + lado3) / 2;
+        double area = 0;
+        if(verificaTriangulo(lado1,lado2,lado3)){
+            area = Math.sqrt(semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3));
+        }
 
-
-        double area = Math.sqrt(semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3));
 
         return area;
     }
+    public static boolean verificaTriangulo (double lado1, double lado2, double lado3){
+        boolean condicao1 = lado1 > Math.abs(lado2 - lado3) && lado1 < lado2 + lado3;
+        boolean condicao2 = lado2 > Math.abs(lado1 - lado3) && lado2 < lado1 + lado3;
+        boolean condicao3 = lado3 > Math.abs(lado1 - lado2) && lado3 < lado1 + lado2;
+        return condicao1 && condicao2 && condicao3;
+    }
+
 
 
 }
